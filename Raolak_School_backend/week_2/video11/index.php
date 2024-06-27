c6e2fbe5a2ec38d2b0a387ac6d845e5060f23ceb
adedeni <161731890+adedeni@users.noreply.github.com>
2024-06-27 13:37:07 +0100

    l

--- Raolak_School_backend/week_2/video11/index.php
+++ Raolak_School_backend/week_2/video11/index.php
@@ -7,7 +7,33 @@
 </head>
 <body>
     <?php 
-    
+    $string = "we dey outside";
+
+    echo strlen($string);
+    ?>
+    <br>
+    <?php 
+    $string = "we dey outside";
+
+    echo strpos($string, "i");
+    ?>
+    <br>
+    <?php 
+    $string = "we dey outside";
+
+    echo str_replace("outside", "inside", $string,);
+    ?>
+    <br>
+    <?php 
+    $string = "we dey outside";
+
+    echo strtoupper($string);//uppercase or lower case
+    ?>
+     <br>
+    <?php 
+    $string = "we dey outside";
+
+    echo substr($string, 1, -3);
     ?>
 </body>
 </html>
