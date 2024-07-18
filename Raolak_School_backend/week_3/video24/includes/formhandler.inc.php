c31440212303bf393ee141ccb7828cc73aeebda0
adedeni <161731890+adedeni@users.noreply.github.com>
2024-07-18 15:33:32 +0100

    djh

--- Raolak_School_backend/week_3/video24/includes/formhandler.inc.php
+++ Raolak_School_backend/week_3/video24/includes/formhandler.inc.php
@@ -2,16 +2,17 @@
 if ($_SERVER ["REQUEST_METHOD"] == "POST") {
 $username = $_POST["username"];
 $pwd = $_POST["pwd"];
-
+$email = $_POST["email"];
 
 try {
-    require_once "dbh.inc.php";
+    require_once "dbh.inc.php";//linking to another file
 
-    $query = "DELETE FROM users WHERE username = :username AND pwd = :pwd;";
+    $query = "INSERT INTO users (username, pwd, email) VALUES (:username, :pwd, :email);";
     
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(":username", $username);
     $stmt->bindParam(":pwd", $pwd);
+    $stmt->bindParam(":email", $email);
     $stmt->execute();
     
     $pdo = null;
