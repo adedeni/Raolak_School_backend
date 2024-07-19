ff794397ac9858e4cd3faa01e615ca39e1d5213d
adedeni <161731890+adedeni@users.noreply.github.com>
2024-07-19 16:42:31 +0100

    djksd

--- Raolak_School_backend/week_3/video24/search.php
+++ Raolak_School_backend/week_3/video24/search.php
@@ -5,7 +5,7 @@ $userSearch = $_POST["usersearch"];
 try {
     require_once "includes/dbh.inc.php";
 
-    $query = "SELECT * FROM comments WHERE  username = usersearch;";
+    $query = "SELECT * FROM comments WHERE  username = :usersearch;";
     
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(":usersearch", $userSearch);
@@ -13,8 +13,6 @@ try {
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);//this is to help us grab the data and make it equals to array in our code
     $pdo = null;
     $stmt = null;
-    
-    die ();
 } catch (PDOException $e) {
     die("Query failed: " . $e->getMessage());
 }
@@ -31,12 +29,25 @@ try {
     <title>Document</title>
 </head>
 <body>
+    <section><!--this is for styling-->
     <H3>Search Results:</H3>
     <?php
     if (empty($results)) {
-
+        echo "<div>";
+        echo "please enter search";
+        echo "</div>";
+//this if statement is to act has error handler to ensure a username that is not in our database does get a error response and if a user does not make a comment they would get no comment response
+    } else {
+        foreach ($results as $row) {
+            echo "<div>";
+            echo "<h3>" . htmlspecialchars($row ["username"]) . "</h3>";
+            echo "<h4>" . htmlspecialchars($row ["comment_text"]) . "</h4>";
+            echo "<p>" . htmlspecialchars($row ["created_at"]) . "</p>";
+            echo "</div>";//htmlspecial character must be used if you are printing or echoing a userdata on the website from users query otherwise no need
+        }
     }
-    //this if statement is to act has error handler to ensure a username that is not in our database does get a error response and if a user does not make a comment they would get no comment response
+    
     ?>
+    </section>
 </body>
 </html>
