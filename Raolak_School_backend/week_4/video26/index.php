72c12bbed8d80772233d2b779206e08f858fa2d2
adedeni <161731890+adedeni@users.noreply.github.com>
2024-07-24 18:42:00 +0800

    gfj

--- Raolak_School_backend/week_4/video26/index.php
+++ Raolak_School_backend/week_4/video26/index.php
@@ -1,13 +1,15 @@
+<?php
+    //this is a lession on how to prevent computers/user from accessing our session data
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <?php
-    //this is a lession on how to prevent computers/user from accessing our session data
-    ?>
+    
 </body>
 </html>
